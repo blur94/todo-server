f9b587aee07d8b3f2433ae0b0f10ea1f85280bb7
Gilead Odo <odogilead@gmail.com>
2024-02-16 23:07:58 +0100

    Prisma schema loaded from prisma/schema.prisma

--- src/utils/todoFunc.ts
+++ src/utils/todoFunc.ts
@@ -1,29 +1,24 @@
 "use server";
-
 import { prisma } from "@/db";
+
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export const getTodos = async () => {
-  "use server";
   return prisma.todo.findMany();
 };
 
 export const toggleTodo = async (id: string, complete: boolean) => {
-  "use server";
   await prisma.todo.update({ where: { id }, data: { complete } });
 };
 
 export const deleteTodo = async (id: string) => {
-  "use server";
   await prisma.todo.delete({ where: { id } });
 
   revalidatePath("/");
 };
 
 export const createTodo = async (data: FormData) => {
-  "use server";
-
   const title = data.get("title")?.valueOf();
 
   if (typeof title !== "string" || title.length < 1) {
@@ -37,12 +32,10 @@ export const createTodo = async (data: FormData) => {
 };
 
 export const getTodo = async (id: string) => {
-  "use server";
   return await prisma.todo.findUnique({ where: { id } });
 };
 
 export const handleEditTodo = async (id: string, data: FormData) => {
-  "use server";
   const title = data.get("title")?.valueOf();
   if (typeof title !== "string" || title.length < 1) {
     throw new Error("Title is required");
